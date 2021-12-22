13ee541ef2ff335d571752584cf326b831a553bc
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-12-22 12:24:41 +0100

    vue practise

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+<ul>
+
+
+</ul>
+</template>
+
+<script>
+export default({
+    data() {
+        // storedResources: [
+        //     { id: 'official-guide', title: 'Official Guide', description: 'The official Vue.js doc',
+        //     link: 'https://vuejs.org'},
+        //     { id: 'google', title: 'Google', description: 'Learn to Google',
+        //     link: 'https://google.com'}
+        // ]
+        
+    }
+})
+</script>
+
+<style> 
+
+</style>
